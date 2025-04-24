9d0cd34f9789b9110b60929eb240a03d0696d8fc
Jida Li <77774296+jidalii@users.noreply.github.com>
2025-04-24 14:52:43 -0400

    refactored user input

--- ocr/run_pipeline.py
+++ ocr/run_pipeline.py
@@ -6,44 +6,54 @@ then outputs results as JSON files.
 
 import os
 import shutil
+
+from doctr.models import ocr_predictor
+
 from ocr.pipeline.split_pages import process_pdf
 from ocr.pipeline.pdf_to_cells import pdf_to_cells
 from ocr.pipeline.cells_to_contents import extract_contents
 from ocr.utilities.dir_helper import create_dir_if_not_exists, get_files_in_directory
-
-from doctr.models import ocr_predictor
 from utilities.load_config import load_table_config, load_section_config
-
-# Directory containing input PDF forms
-input_dir = "../files"
-# Directory to store result JSON files
-res_dir = os.path.join(input_dir, "results")
-
-# Paths to configuration files (do not modify)
-table_config_path = "config/table_config.yaml"
-section_config_path = "config/section_config.yaml"
-
-# Choose form type: EEO-1 or EEO-5
-FORM_TYPE = "eeo1"
-# FORM_TYPE = "eeo5"
-
-# Load specific form and checkbox configs based on form type
-if FORM_TYPE == "eeo1":
-    form_config = "config/eeo1_typed_type1.yaml"
-    checkbox_config = "config/eeo1_typed_type1_checkbox.yaml"
-    PAGE_NUM_LS = [0]  # Pages to process for EEO-1
-elif FORM_TYPE == "eeo5":
-    form_config = "config/eeo5_typed.yaml"
-    checkbox_config = "config/eeo1_typed_type1_checkbox.yaml"  # Common checkbox config
-    PAGE_NUM_LS = [0, 1]  # Pages to process for EEO-5
-else:
-    raise Exception(f"Invalid FORM_TYPE: {FORM_TYPE}")
 
 
 def main():
     """
     Main function to initialize OCR predictor and process all PDFs.
     """
+    
+    # ===================================> User Input Starts <===================================
+    # Directory containing input PDF forms
+    input_dir = input("Enter the input directory: ")
+    # Directory to store result JSON files
+    res_dir = input(
+        'Enter the output directory (if empty, output_dir is "`input_dir`/results"): '
+    )
+    if res_dir == "":
+        res_dir = os.path.join(input_dir, "results")
+
+    # Choose form type: EEO-1 or EEO-5
+    FORM_TYPE = input("Enter the form type (eeo1 or eeo5): ")
+
+    # Paths to configuration files (do not modify)
+    table_config_path = "config/table_config.yaml"
+    section_config_path = "config/section_config.yaml"
+    
+    # ===================================> User Input Ends <===================================
+
+    # Load specific form and checkbox configs based on form type
+    if FORM_TYPE == "eeo1":
+        form_config = "config/eeo1_typed_type1.yaml"
+        checkbox_config = "config/eeo1_typed_type1_checkbox.yaml"
+        PAGE_NUM_LS = [0]  # Pages to process for EEO-1
+    elif FORM_TYPE == "eeo5":
+        form_config = "config/eeo5_typed.yaml"
+        checkbox_config = (
+            "config/eeo1_typed_type1_checkbox.yaml"  # Common checkbox config
+        )
+        PAGE_NUM_LS = [0, 1]  # Pages to process for EEO-5
+    else:
+        raise Exception(f"Invalid FORM_TYPE: {FORM_TYPE}")
+
     # Initialize the OCR predictor with specified architectures
     predictor = ocr_predictor(
         det_arch="fast_base",
